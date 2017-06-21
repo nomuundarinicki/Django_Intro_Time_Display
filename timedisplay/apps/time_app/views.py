d45613c58adb584088b63ccd611ee6fba18d21cf
Nick Chimedbavuu <nickchimedbavuu@Nicks-MacBook-Pro.local>
2017-06-21 08:43:07 -0700

    djangointro

---
+++ timedisplay/apps/time_app/views.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+
+from django.shortcuts import render, HttpResponse
+import datetime
+
+# Create your views here.
+def index(request):
+    now = datetime.datetime.now()
+    context = {
+    "date": now.strftime('%b %d, %Y'),
+    "time": now.strftime('%I:%M %p'),
+
+    }
+    return render(request, 'time_app/index.html', context)
